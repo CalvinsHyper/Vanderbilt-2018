ced97efa939d851791a326c722c91f891c482050
CalvinsHyper <39976459+CalvinsHyper@users.noreply.github.com>
2018-06-11 14:34:36 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -13,3 +13,23 @@
 
 
 # If you complete extensions, describe your extensions here!
+print "Hello"
+Your_Name = raw_input("What's your name?")
+print "Your name is "+ Your_Name
+Your_Grade = raw_input("What Grade are you in?")
+print "you are in"+ Your_Grade
+x = 16-int(Your_Grade)
+print "you wil graduate in" +str(x) +  "Years"
+
+print "Part II"
+
+Your_Month = raw_input("what is your birth month?")
+Your_Day = raw_input("what day of the month is your Birthday?")
+if Your_Month < 6
+
+
+
+
+
+
+

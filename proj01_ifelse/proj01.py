f33ba3945a57f96fd21c0849f01c587869f2fd24
CalvinsHyper <39976459+CalvinsHyper@users.noreply.github.com>
2018-06-12 09:51:20 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -6,13 +6,6 @@
 # Part I:
 # This program asks the user for his/her name and grade.
 #Then, it prints out a sentence that says the number of years until they graduate.
-
-# Part II:
-# This program asks the user for his/her name and birth month.
-# Then, it prints a sentence that says the number of days and months until their birthday
-
-
-# If you complete extensions, describe your extensions here!
 print "Hello"
 Your_Name = raw_input("What's your name?")
 print "Your name is "+ Your_Name
@@ -21,25 +14,36 @@ print "you are in"+ Your_Grade
 x = 16-int(Your_Grade)
 print "you wil graduate in" +str(x) +  "Years"
 
+# Part II:
+# This program asks the user for his/her name and birth month.
+# Then, it prints a sentence that says the number of days and months until their birthday
 print "Part II"
-Current_Month = raw_input("what is the current month NUMBER")
-Current_Day = raw_input("What is the current day NUMBER")
-Your_Month = raw_input("what is your birth month NUMBER?")
-Your_Day = raw_input("what day of the month is your Birthday NUMBER?")
-q= Your_Month-Current_Month
-w=12-(Current_Month-Your_Month)
-e=Your_Day-Current_Day
-r=30-(Current_Day-Your_Day)
+Current_Month = int(raw_input("what is the current month NUMBER"))
+Current_Day = int(raw_input("What is the current day NUMBER"))
+Your_Month = int(raw_input("what is your birth month NUMBER?"))
+Your_Day = int(raw_input("what day of the month is your Birthday NUMBER?"))
+q = (Your_Month-Current_Month)
+w = (12-Current_Month-Your_Month)
+e = (Your_Day-Current_Day)
+r = (30-Current_Day-Your_Day)
 if Your_Month>Current_Month:
-    print "the number of months until your bday is " +str( q)
+    print "the number of days until your bday is " + str( q)
 else:
-    print"the number of months until your bday is "  +str( w)
-
+    print"the number of days until your bday is "  + str( w)
 if Your_Day >= Current_Day:
-    print "the number of days until your bday is"  +str(e)
-
+    print "the number of months until your bday is"  + str(e)
 else:
-    print "The number of days until your birthday is" +str(r)
+    print "The number of months until your birthday is" + str(r)
+
+# If you complete extensions, describe your extensions here!
+Your_Age=int(raw_input("how old are you"))
+if Your_Age<13:
+    print ("you may only see G and PG movies")
+if Your_Age>13:
+    print ("You can see any movies except for R movies")
+if Your_Age>17:
+    print ("you can watch any rated movie")
+
 
 
 

f1cc22e2dc868e142074384f3254587febc35ed5
CalvinsHyper <39976459+CalvinsHyper@users.noreply.github.com>
2018-06-13 10:44:02 -0500

    Update proj03.py

--- proj03_guessing_game/proj03.py
+++ proj03_guessing_game/proj03.py
@@ -1,5 +1,35 @@
 # Name:
 # Date:
+import random
+
+print("Guessing Game!")
+print("(type exit to end game)")
+guess = input("Guess a number between 1 and 9: ")
+random_number = random.randint(1,9)
+amntguess = 0
+while int(guess) != random_number and int(guess) != 0 or amntguess < 4:
+    if int(guess) < random_number:
+        print("That is too low!")
+        amntguess = amntguess + 1
+        guess = input("Guess a number between 1 and 9: ")
+        if guess == "exit" or amntguess == 4:
+            guess = 0
+            print("The game has been Terminated.")
+    elif int(guess) > random_number:
+        print("That is too high!")
+        amntguess = amntguess + 1
+        guess = input("Guess a number between 1 and 9: ")
+        if guess == "exit" or amntguess == 4:
+            guess = 0
+            print("The game has been Terminated.")
+    else:
+        amntguess = amntguess + 1
+        print("You got it in " + str(amntguess) + " guesses!")
+    #if  int(guess) > 9:
+        #print("No don't do that.")
+        #guess = 0
+
+
 
 
 """ 
